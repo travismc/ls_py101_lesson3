1283b3c1dd5cb3c8ed56b5e7be7c7c0b395f486e
Travis M <travis.mchugh@gmail.com>
2024-10-01 20:04:13 -0700

    Added solution to PY101 Lesson 3, Easy 1, Question 4

---
+++ munsters.py
@@ -0,0 +1,9 @@
+# PY101, Lesson 3, Question 4
+
+
+munsters_description = "the Munsters are CREEPY and Spooky."
+# => 'The munsters are creepy and spooky.'
+
+# print(munsters_description.lower().capitalize())
+# Below is more succinct
+print(munsters_description.capitalize())
