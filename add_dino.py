c092365af2cc343bae9e2b8e22eb1ee5c9993327
Travis M <travis.mchugh@gmail.com>
2024-10-02 17:21:02 -0700

    Added solution to PY101 Lesson 3, Easy 1, Question 7

---
+++ add_dino.py
@@ -0,0 +1,8 @@
+# How can we add the family pet, "Dino", to the following list?
+
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"]
+
+flintstones.append("Dino")
+
+print(flintstones)
