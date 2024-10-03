14bc1dbaf9a459a9f47aff229c7857ed43f7fb18
Travis M <travis.mchugh@gmail.com>
2024-10-02 18:44:08 -0700

    Added solution to PY101 Lesson 3, Easy 1, Question 10

---
+++ urgent.py
@@ -0,0 +1,10 @@
+# Print the following string with the word important replaced
+# by urgent:
+
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+# can be reassigned to advice instead of creating new variable for new string
+urgent_advice = advice.replace("important", "urgent")
+
+print(urgent_advice)
