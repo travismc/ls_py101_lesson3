a2d748d82bc264e116f50ff7939887045a13734b
Travis M <travis.mchugh@gmail.com>
2024-10-04 16:36:29 -0700

    Added solution to PY101 Lesson 3, Easy 2, Question 7

---
+++ count.py
@@ -0,0 +1,10 @@
+# Write a one-liner to count the number of lower-case t characters
+# in each of the following strings:
+
+
+statement1 = "The Flintstones Rock!"
+statement2 = "Easy come, easy go."
+
+print(f"statement1 has {statement1.count('t')} occurrences of 't'.")
+
+print(f"statement2 has {statement2.count('t')} occurrences of 't'.")
