1838f7f9363bec0ff6b2ee369153c70bce9c62c7
Travis M <travis.mchugh@gmail.com>
2024-10-04 16:16:46 -0700

    Added solution to PY101 Lesson 3, Easy 2, Question 4

---
+++ index2.py
@@ -0,0 +1,9 @@
+# Given a list of numbers [1, 2, 3, 4, 5], mutate the
+# list by removing the number at index 2, so that the
+# list becomes [1, 2, 4, 5].
+
+nums = [1, 2, 3, 4, 5]
+
+del nums[2]
+
+print(nums)
