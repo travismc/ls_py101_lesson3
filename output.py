843be5bd71125bb312a4e41dbbc5bf60a5a6cf9a
Travis M <travis.mchugh@gmail.com>
2024-10-05 09:09:00 -0700

    Added solution to PY101 Lesson 3, Hard 1, Question 3

---
+++ output.py
@@ -0,0 +1,69 @@
+# Example A
+def mess_with_vars(one, two, three):
+    one = two
+    two = three
+    three = one
+
+
+one = ["one"]
+two = ["two"]
+three = ["three"]
+
+mess_with_vars(one, two, three)
+
+print(f"one is: {one}")
+print(f"two is: {two}")
+print(f"three is: {three}")
+
+# Anticipated output will be: ['one'] ['two'] ['three'];
+# the mess_with_vars function changes values in local scope
+# but does not return anything, so no global variable reassignment.
+
+
+# Example B
+
+def mess_with_vars(one, two, three):
+    one = ["two"]
+    two = ["three"]
+    three = ["one"]
+
+
+one = ["one"]
+two = ["two"]
+three = ["three"]
+
+mess_with_vars(one, two, three)
+
+print(f"one is: {one}")
+print(f"two is: {two}")
+print(f"three is: {three}")
+
+# Again, anticipated output will be: ['one'] ['two'] ['three'];
+# the mess_with_vars function changes values in local scope
+# but does not return anything, so no global variable reassignment.
+
+
+# Example C
+
+def mess_with_vars(one, two, three):
+    one[0] = "two"
+    two[0] = "three"
+    three[0] = "one"
+
+
+one = ["one"]
+two = ["two"]
+three = ["three"]
+
+mess_with_vars(one, two, three)
+
+print(f"one is: {one}")
+print(f"two is: {two}")
+print(f"three is: {three}")
+
+# Again, anticipated output will be: ['one'] ['two'] ['three'];
+# the mess_with_vars function changes values in local scope
+# but does not return anything, so no global variable reassignment.
+# HOWEVER- the mess_with_vars function modifies the content of the
+# lists directly. Since lists in Python are mutable and passed by
+# reference, the changes are reflected outside the function.
