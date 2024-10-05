4cf83687a607597e983d466b19a345be7f4c27c0
Travis M <travis.mchugh@gmail.com>
2024-10-04 17:20:56 -0700

    Added solution to PY101 Lesson 3, Easy 2, Question 8

---
+++ spot.py
@@ -0,0 +1,6 @@
+# Determine whether the following dictionary of people and their
+# age contains an entry for 'Spot':
+
+ages = {'Herman': 32, 'Lily': 30, 'Grandpa': 402, 'Eddie': 10}
+
+print('Spot' in ages)
