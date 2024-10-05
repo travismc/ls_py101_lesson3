dfa80430f2cc1d3707c0c1154c7db51e69a9aa35
Travis M <travis.mchugh@gmail.com>
2024-10-04 20:47:10 -0700

    Added solution to PY101 Lesson 3, Medium 1, Question 2

---
+++ factors.py
@@ -0,0 +1,12 @@
+def factors(number):
+    divisor = number
+    result = []
+    while divisor > 0:
+        if number % divisor == 0:
+            result.append(number // divisor)
+        divisor -= 1
+    return result
+
+
+print(factors(4))
+print(factors(-4))
